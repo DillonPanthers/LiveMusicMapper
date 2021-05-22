b326ad5566a0aa4e43529d16090316453a2c4887
Craig Ferreira <ferreira.craig@gmail.com>
2021-05-22 15:32:59 -0400

    initiated repo

--- server/server.js
+++ server/server.js
@@ -4,9 +4,9 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 
 // Static Files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname,'..', 'public')));
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
 });
 
 app.listen(port, () => {
