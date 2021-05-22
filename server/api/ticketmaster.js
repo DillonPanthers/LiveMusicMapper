f37faccedeb29e36d20e2d3fa357bd4a2703f416
Vikki Chan <vikkiwchan@gmail.com>
2021-05-22 16:35:55 -0500

    added map

--- server/api/ticketmaster.js
+++ server/api/ticketmaster.js
@@ -1,23 +1,24 @@
-const router = require('express').Router()
-const axios= require('axios')
-const dotenv= require('dotenv')
-dotenv.config()
+const router = require('express').Router();
+const axios = require('axios');
+const dotenv = require('dotenv');
+dotenv.config();
 
+// GET /api/ticketmaster
+router.get('/', async (req, res) => {
+  try {
+    //TICKET MASTER
+    // const data = await axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=${process.env.TICKETMASTERAPIKEY}`)
+    // const events = await axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&countryCode=US&apikey=${process.env.TICKETMASTERAPIKEY}`)
 
-router.get('/', async(req, res)=>{
-    try{
-        //TICKET MASTER
-        // const data = await axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=${process.env.TICKETMASTERAPIKEY}`)
-        // const events = await axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&countryCode=US&apikey=${process.env.TICKETMASTERAPIKEY}`)
-        
-        const events = await axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&countryCode=CA&apikey=${process.env.TICKETMASTERAPIKEY}`)
-        console.log('full events object', events)
-        console.log('data', events.data._embedded.events)
-        res.send(events.data._embedded.events)
-    }catch(err){
-        console.log(err)
-    }
-})
+    const events = await axios.get(
+      `https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&countryCode=CA&apikey=${process.env.TICKETMASTERAPIKEY}`
+    );
+    console.log('full events object', events);
+    console.log('data', events.data._embedded.events);
+    res.send(events.data._embedded.events);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-
-module.exports =router
+module.exports = router;
