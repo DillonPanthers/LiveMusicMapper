4812346924286610b5b081844a04fbc16afd3baf
Inderprit Singh <singhinderprit1992@gmail.com>
2021-05-24 21:49:34 -0400

    cleaned up console.logs

--- client/Components/Map.jsx
+++ client/Components/Map.jsx
@@ -13,91 +13,19 @@ class Map extends React.Component{
   }
   async componentDidMount(){
     navigator.geolocation.getCurrentPosition((position)=>{
-      console.log("Latitude is :", position.coords.latitude);
-      console.log("Longitude is :", position.coords.longitude);
       this.setState({
         lat: position.coords.latitude,
         lon: position.coords.longitude
       })
     });
-    // const latlong = this.state.lat +","+this.state.lon;
-    console.log(typeof latlong, "latitude longitude here SIR")
     const ticketDataByLocation = await axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&lat=${this.state.lat}&long=${this.state.lon}&apikey=${TICKETMASTERAPIKEY}`)
-    console.log("ticket data here", ticketDataByLocation)
-
 
     this.setState({ticketDataByLocation: ticketDataByLocation.data._embedded.events})
-    console.log(this.state.ticketDataByLocation, 'componentdidmount check for ticket data')
-//   $.ajax({
-//     type: 'GET',
-//     url:
-//       `https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&countryCode=CA&apikey={TICKETMASTERAPIKEY}`,
-//       // latlon,
-//     async: true,
-//     dataType: 'json',
-//     success: function (json) {
-//       console.log(json);
-//       var e = document.getElementById('events');
-//       e.innerHTML = json.page.totalElements + ' events found.';
-//       showEvents(json);
-//       initMap(position, json);
-//     },
-//     error: function (xhr, status, err) {
-//       console.log(err);
-//     },
-//   });
-//   return latlon
-// }
 
-
-
-// function showEvents(json) {
-//   for (var i = 0; i < json.page.size; i++) {
-//     $('#events').append('<p>' + json._embedded.events[i].name + '</p>');
-//   }
-// }
-
-// function initMap(position, json) {
-//   var mapDiv = document.getElementById('map');
-//   var map = new google.maps.Map(mapDiv, {
-//     center: { lat: position.coords.latitude, lng: position.coords.longitude },
-//     zoom: 10,
-//   });
-//   for (var i = 0; i < json.page.size; i++) {
-//     addMarker(map, json._embedded.events[i]);
-//   }
-// }
-
-// function addMarker(map, event) {
-//   var marker = new google.maps.Marker({
-//     position: new google.maps.LatLng(
-//       event._embedded.venues[0].location.latitude,
-//       event._embedded.venues[0].location.longitude
-//     ),
-//     map: map,
-//   });
-//   marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
-//   console.log(marker);
-// }
-
-
-  /////////
-
-  
-
-
-
-
-
-
-
-    // getLocation();
-    // console.log('lat lon',latlon)
   }
 
   render() {
-    console.log('state', this.state)
-    // console.log(this.state.ticketDataByLocation,'ticket data in render method')
+
     return (
         <LoadScript googleMapsApiKey={REACT_APP_GOOGLEAPIKEY}>
         <GoogleMap
@@ -111,8 +39,8 @@ class Map extends React.Component{
                   lng: this.state.lon,
                 }} 
               />
-              {this.state.ticketDataByLocation.map((currEvent, idx) => {
-               return <Marker key={idx}
+              {this.state.ticketDataByLocation.map((currEvent) => {
+               return <Marker key={currEvent.id}
                 position={{
                   lat: +currEvent._embedded.venues[0].location.latitude,
                   lng: +currEvent._embedded.venues[0].location.longitude,
