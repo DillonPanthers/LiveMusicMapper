07737a68f5e70f7ba92814b7a2d1c0ccc682404f
Vikki Chan <vikkiwchan@gmail.com>
2021-05-26 18:23:53 -0500

    fixed CSS inline styling on MUI components

--- client/Components/Main.jsx
+++ client/Components/Main.jsx
@@ -5,20 +5,16 @@ import NavBar from './NavBar';
 import LandingPage from './LandingPage/LandingPage.jsx';
 import Map from './Map';
 
-class Main extends React.Component {
-  render() {
-    return (
-      <div>
-        <Router>
-          <NavBar />
-          <Switch>
-            <Route component={LandingPage} path="/" exact />
-            <Route component={Map} path="/map" exact />
-          </Switch>
-        </Router>
-      </div>
-    );
-  }
-}
+const Main = () => (
+  <div>
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route component={LandingPage} path="/" exact />
+        <Route component={Map} path="/map" exact />
+      </Switch>
+    </Router>
+  </div>
+);
 
 export default Main;
