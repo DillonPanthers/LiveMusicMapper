b9a36317104962021e9569e2ac5022056b65f4b5
Vikki Chan <vikkiwchan@gmail.com>
2021-05-25 19:18:14 -0500

    started working on friend

--- server/db/relationships.js
+++ server/db/relationships.js
@@ -1,9 +1,13 @@
 //Import all models from models folder here
 const { Concert } = require('./models/Concert');
+const { User } = require('./models/User');
+const { Friend } = require('./models/Friend');
 
 /*
 Relationships Here
 */
+User.hasMany(Friend, { through: 'Users_Friends' });
+Friend.belongsToMany(User, { through: 'Users_Friends' });
 
 //After relationships are created, export them here into hooks.js
-module.exports = { Concert };
+module.exports = { Concert, User, Friend };
