592ac7806a413abce1026a920fc122b7a68257fa
Vikki Chan <vikkiwchan@gmail.com>
2021-05-26 15:28:30 -0500

    seeded admin users

--- bin/seed.js
+++ bin/seed.js
@@ -1,9 +1,23 @@
-const { db, Concert, Genre } = require('../server/db');
+const { db, Concert, Genre, User } = require('../server/db/index'); // import models from index so we have access to hooks and magic methods
+
+const { users } = require('./data/users');
 
 const init = async () => {
   try {
     await db.sync({ force: true });
+    await Promise.all(
+      users.map((user) =>
+        User.create({
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email: user.email,
+          password: user.password,
+          isAdmin: user.isAdmin,
+        })
+      )
+    );
     console.log('connected');
+    await db.close();
   } catch (error) {
     console.log(error);
   }
--- server/db/relationships.js
+++ server/db/relationships.js
@@ -1,24 +1,11 @@
 //Import all models from models folder here
 const { Concert } = require('./models/Concert');
-<<<<<<< HEAD
-const { User } = require('./models/User');
-const { Friend } = require('./models/Friend');
-
-/*
-Relationships Here
-*/
-User.hasMany(Friend, { through: 'Users_Friends' });
-Friend.belongsToMany(User, { through: 'Users_Friends' });
-
-//After relationships are created, export them here into hooks.js
-module.exports = { Concert, User, Friend };
-=======
 const { Genre } = require('./models/Genre');
+const { User } = require('./models/User');
 
 //Relationships Here
 Concert.belongsToMany(Genre, { through: 'concertgenres', timestamps: false });
 Genre.belongsToMany(Concert, { through: 'concertgenres', timestamps: false });
 
 //After relationships are created, export them here into hooks.js
-module.exports = { Concert, Genre };
->>>>>>> development
+module.exports = { Concert, Genre, User };
