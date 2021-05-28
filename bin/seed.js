505147203602b7eca32b02b04dc9fa515bea471a
Alejandra Villanueva <alejyvilla@gmail.com>
2021-05-27 20:25:27 -0400

    Removed Test Seed

--- bin/seed.js
+++ bin/seed.js
@@ -1,42 +1,34 @@
 const {
-  db,
-  Concert,
-  Genre,
-  User,
-  Friendship,
-  FriendRequest,
+    db,
+    Concert,
+    Genre,
+    User,
+    Friendship,
+    FriendRequest,
 } = require('../server/db/index'); // import models from index so we have access to hooks and magic methods
 
 const { users } = require('./data/users');
 
 const init = async () => {
-  try {
-    await db.sync({ force: true });
-    const [vikki, alejandra, inderprit, craig] = await Promise.all(
-      users.map((user) =>
-        User.create({
-          firstName: user.firstName,
-          lastName: user.lastName,
-          email: user.email,
-          password: user.password,
-          isAdmin: user.isAdmin,
-        })
-      )
-    );
+    try {
+        await db.sync({ force: true });
+        const [vikki, alejandra, inderprit, craig] = await Promise.all(
+            users.map((user) =>
+                User.create({
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    email: user.email,
+                    password: user.password,
+                    isAdmin: user.isAdmin,
+                })
+            )
+        );
 
-    await vikki.addFriend([alejandra, inderprit]);
-    const us = await User.findAll({
-      include: 'friends',
-    });
-
-    await vikki.addInvitees(alejandra);
-    console.log(await alejandra.getRequestedBy());
-
-    console.log('connected');
-    await db.close();
-  } catch (error) {
-    console.log(error);
-  }
+        console.log('connected');
+        await db.close();
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 init();
