860f22adc9513bb8de75c929b7a9094b1f8bcf8e
Vikki Chan <vikkiwchan@gmail.com>
2021-06-03 00:01:02 -0500

    added comments for creating new users

--- server/api/user.js
+++ server/api/user.js
@@ -3,16 +3,18 @@ const router = require('express').Router();
 const { Concert } = require('../db/index');
 const { requireToken } = require('./utils/utils');
 
-// TODO: Probably don't need this since concerts and friends will be eager loaded with user in auth route
+// TODO: add a route to create a user
+
+// NOTE: this may not be required bc we are eagerloading concerts with the user model
 
 // GET /api/user/:id/concerts
-router.get('/:id/concerts', requireToken, async (req, res, next) => {
-    try {
-        res.send(await Concert.byToken(req.user, req.params.id));
-    } catch (ex) {
-        next(ex);
-    }
-});
+// router.get('/:id/concerts', requireToken, async (req, res, next) => {
+//     try {
+//         res.send(await Concert.byToken(req.user, req.params.id));
+//     } catch (ex) {
+//         next(ex);
+//     }
+// });
 
 // POST /api/user/ - create user
 
