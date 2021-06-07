e1975ae0a2e4ff01590e72aca60ea4a3c345e54d
Inderprit Singh <singhinderprit1992@gmail.com>
2021-06-07 15:46:42 -0400

    minor material ui updates

--- client/Components/User/FriendRequests.js
+++ client/Components/User/FriendRequests.js
@@ -1,11 +1,28 @@
 import React, { useEffect, useContext, useState } from 'react';
 import axios from 'axios';
+import { Typography, Button, Container, makeStyles } from '@material-ui/core';
+
 import { GlobalState } from '../../contexts/Store';
+
+const useStyles = makeStyles((theme) => ({
+    margin: theme.spacing(1),
+    button: {
+        color: 'white',
+        backgroundColor: 'gray',
+        maxHeight: '20px',
+        marginRight: '5px',
+        marginLeft: '5px',
+    },
+}));
+
+//TODO: Do we really need a block button? Is Add or Remove enough?
 
 function FriendRequests() {
     const { auth } = useContext(GlobalState);
     const [user] = auth;
     const [friendRequests, setFriendRequests] = useState([]);
+
+    const classes = useStyles();
 
     useEffect(() => {
         const getFriendRequests = async () => {
@@ -31,25 +48,37 @@ function FriendRequests() {
         console.log('BLOCK ME');
     };
 
+    //TODO: Get button css working
     return (
-        <div>
+        <Container>
             {friendRequests.map((request) => {
                 console.log(request);
 
                 return (
-                    <div key={request.userId}>
-                        {request.userInfo.fullName}{' '}
-                        <button
+                    <Container key={request.userId}>
+                        <Typography>{request.userInfo.fullName}</Typography>
+                        <Button
+                            className={classes.button}
                             onClick={() => addFriend(request.userId, user.id)}
                         >
                             Add
-                        </button>
-                        <button onClick={() => ignoreFriend()}>Ignore</button>
-                        <button onClick={() => blockFriend()}>Block</button>
-                    </div>
+                        </Button>
+                        <Button
+                            onClick={() => ignoreFriend()}
+                            className={classes.button}
+                        >
+                            Ignore
+                        </Button>
+                        <Button
+                            onClick={() => blockFriend()}
+                            className={classes.button}
+                        >
+                            Block
+                        </Button>
+                    </Container>
                 );
             })}
-        </div>
+        </Container>
     );
 }
 
