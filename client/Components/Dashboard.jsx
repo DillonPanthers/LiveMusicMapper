d60b9e1a8c2aab71aad9068752ecd32de16d5c4f
Inderprit Singh <singhinderprit1992@gmail.com>
2021-06-11 02:08:48 -0400

    minor linting

--- client/Components/Dashboard.jsx
+++ client/Components/Dashboard.jsx
@@ -10,18 +10,18 @@ import {
 import { GlobalState } from '../contexts/Store';
 import { REACT_APP_GOOGLEAPIKEY } from '../secret';
 
-//TODO: Sort the dates of the upcoming concerts, maybe use a util function. 
-//TODO: Use avatars as a grid to show some friends, and a link to all friends 
+//TODO: Sort the dates of the upcoming concerts, maybe use a util function.
+//TODO: Use avatars as a grid to show some friends, and a link to all friends
 
-//TODO: On all friends page we can have all friends, and on a sidebar/side component a view of pending friend requests, and the search component there. If no friend requests, then display message no friends. 
+//TODO: On all friends page we can have all friends, and on a sidebar/side component a view of pending friend requests, and the search component there. If no friend requests, then display message no friends.
 
 //maybe move to utils folder later
 
-const compareDate = (dateOne, dateTwo) =>{
+const compareDate = (dateOne, dateTwo) => {
     const d1 = Date.parse(dateOne.date);
     const d2 = Date.parse(dateTwo.date);
-    return d1-d2; 
-}
+    return d1 - d2;
+};
 
 const Dashboard = () => {
     const { auth, getUserData, location, getUserLocation } =
@@ -33,7 +33,7 @@ const Dashboard = () => {
     const [friends, setFriends] = useState([]);
     const [concerts, setConcerts] = useState([]);
     const [venues, setVenues] = useState({});
-    const [text, setText] = useState("Upcoming Concerts")
+    const [text, setText] = useState('Upcoming Concerts');
     const [markerState, setMarkerState] = useState({
         isOpen: false,
         selectedEventLat: 0,
@@ -59,102 +59,116 @@ const Dashboard = () => {
             setVenues(venueObj(user.concerts));
             setNumConcerts(user.concerts.length);
             getUserLocation();
-            setConcerts(user.concerts.sort(compareDate))
+            setConcerts(user.concerts.sort(compareDate));
         }
     }, [user.id]);
 
-    const onMarkerPopup = (selectedEventLat, selectedEventLong, selectedVenueName, concerts )=>{
+    const onMarkerPopup = (
+        selectedEventLat,
+        selectedEventLong,
+        selectedVenueName,
+        concerts
+    ) => {
         setConcerts(concerts.sort(compareDate));
         setNumConcerts(concerts.length);
-        setText(`Upcoming Concerts At ${selectedVenueName}`)
-        setMarkerState({...markerState,selectedEventLat,selectedEventLong, selectedVenueName, isOpen: true})
-    }
+        setText(`Upcoming Concerts At ${selectedVenueName}`);
+        setMarkerState({
+            ...markerState,
+            selectedEventLat,
+            selectedEventLong,
+            selectedVenueName,
+            isOpen: true,
+        });
+    };
 
-    const onMapClick = ()=>{
-        setMarkerState({...markerState, isOpen: false})
+    const onMapClick = () => {
+        setMarkerState({ ...markerState, isOpen: false });
         setNumConcerts(user.concerts.length);
         setConcerts(user.concerts.sort(compareDate));
-        setText("Upcoming Concerts")
-    }
+        setText('Upcoming Concerts');
+    };
 
     return (
-        <div style={{display:"flex"}}>
+        <div style={{ display: 'flex' }}>
             {/*TO DO: USER LOADING SCREEN*/}
-            <div style={{marginRight: '10px'}}>
-            <LoadScript googleMapsApiKey={REACT_APP_GOOGLEAPIKEY}>
-                <GoogleMap
-                onClick ={onMapClick}
-                    zoom={10}
-                    center={{ lat: userLocation.lat, lng: userLocation.lon }}
-                    mapContainerStyle={{ height: '50vh', width: '50vw' }}
-                >
-                    {venues
-                        ? Object.keys(venues).map((venue, idx) => {
-                              if (venues[venue][0].lat) {
-                                  return (
-                                      <Marker
-                                          key={venues[venue][0].id}
-                                            onClick={() =>
-                                                onMarkerPopup(
-                                                    venues[venue][0].lat, 
-                                                    venues[venue][0].lon,
-                                                    venue,
-                                                    venues[venue]
-                                                )
-                                            }
-                                          position={{
-                                              lat: +venues[venue][0].lat,
-                                              lng: +venues[venue][0].lon,
-                                          }}
-                                      />
-                                  );
-                              }
-                          })
-                        : null}
+            <div style={{ marginRight: '10px' }}>
+                <LoadScript googleMapsApiKey={REACT_APP_GOOGLEAPIKEY}>
+                    <GoogleMap
+                        onClick={onMapClick}
+                        zoom={10}
+                        center={{
+                            lat: userLocation.lat,
+                            lng: userLocation.lon,
+                        }}
+                        mapContainerStyle={{ height: '50vh', width: '50vw' }}
+                    >
+                        {venues
+                            ? Object.keys(venues).map((venue, idx) => {
+                                  if (venues[venue][0].lat) {
+                                      return (
+                                          <Marker
+                                              key={venues[venue][0].id}
+                                              onClick={() =>
+                                                  onMarkerPopup(
+                                                      venues[venue][0].lat,
+                                                      venues[venue][0].lon,
+                                                      venue,
+                                                      venues[venue]
+                                                  )
+                                              }
+                                              position={{
+                                                  lat: +venues[venue][0].lat,
+                                                  lng: +venues[venue][0].lon,
+                                              }}
+                                          />
+                                      );
+                                  }
+                              })
+                            : null}
 
-                    {markerState.isOpen && (
-                        <InfoWindow
-                            position={{
-                                lat: markerState.selectedEventLat,
-                                lng: markerState.selectedEventLong,
-                            }}
-                        >
-                            <div style = {{color:"black"}}>
-                                {/* <Link to={`/venue/${singleVenue.venueData.id}`}> */}
+                        {markerState.isOpen && (
+                            <InfoWindow
+                                position={{
+                                    lat: markerState.selectedEventLat,
+                                    lng: markerState.selectedEventLong,
+                                }}
+                            >
+                                <div style={{ color: 'black' }}>
+                                    {/* <Link to={`/venue/${singleVenue.venueData.id}`}> */}
                                     {markerState.selectedVenueName}
-                                {/* </Link> */}
-                            </div>
-                        </InfoWindow>
-                    )}
-                </GoogleMap>
-            </LoadScript>
+                                    {/* </Link> */}
+                                </div>
+                            </InfoWindow>
+                        )}
+                    </GoogleMap>
+                </LoadScript>
             </div>
             <div>
-            <p>See {user.id ? user.firstName : 'User'}'s events</p>
-            <p>Friends:</p>
-            <ul>
-                {friends.map((friend) =>
-                    friend.friendship.status === 'accepted' ? (
-                        <li key={friend.id}>
-                            <Link to={`/user/${friend.id}`}>
-                                {friend.firstName}
-                            </Link>
-                        </li>
-                    ) : (
-                        <div key={friend.id}></div>
-                    )
-                )}
-            </ul>
-            <p>{`${text} (${numConcerts})`}</p>
-            <ul>
-                {concerts.map((concert) => (
-                    <li
-                        key={concert.id}
-                    >{`${concert.name} (${concert.date})`}</li>
-                ))}
-            </ul>
+                <p>See {user.id ? user.firstName : 'User'}'s events</p>
+                <p>Friends:</p>
+                <ul>
+                    {friends.map((friend) =>
+                        friend.friendship.status === 'accepted' ? (
+                            <li key={friend.id}>
+                                <Link to={`/user/${friend.id}`}>
+                                    {friend.firstName}
+                                </Link>
+                            </li>
+                        ) : (
+                            <div key={friend.id}></div>
+                        )
+                    )}
+                </ul>
+                <p>{`${text} (${numConcerts})`}</p>
+                <ul>
+                    {concerts.map((concert) => (
+                        <li
+                            key={concert.id}
+                        >{`${concert.name} (${concert.date})`}</li>
+                    ))}
+                </ul>
             </div>
-            </div>
+        </div>
     );
 };
 
