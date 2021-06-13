9498bb6c536572016c741dcfcf309e7aad0c4c94
Vikki Chan <vikkiwchan@gmail.com>
2021-06-12 23:20:59 -0500

    added more ticketmaster genres

--- bin/data/genres.js
+++ bin/data/genres.js
@@ -15,6 +15,38 @@ const genres = [
         id: 'KnvZfZ7vAvF',
         name: 'Dance/Electronic',
     },
+    {
+        id: 'KnvZfZ7vAvd',
+        name: 'Blues',
+    },
+    {
+        id: 'KnvZfZ7vAeJ',
+        name: 'Classical',
+    },
+    {
+        id: 'KnvZfZ7vAeE',
+        name: 'Fairs & Festivals',
+    },
+    {
+        id: 'KnvZfZ7vAv1',
+        name: 'Hip-Hop/Rap',
+    },
+    {
+        id: 'KnvZfZ7vAJ6',
+        name: 'Latin',
+    },
+    {
+        id: 'KnvZfZ7vAvt',
+        name: 'Metal',
+    },
+    {
+        id: 'KnvZfZ7vAvl',
+        name: 'Other',
+    },
+    {
+        id: 'World',
+        name: 'KnvZfZ7vAeF',
+    },
 ];
 
 module.exports = { genres };
