70e5d24d7a707a80b7962c5512d83e37e41b2ba9
Alejandra Villanueva <alejyvilla@gmail.com>
2021-06-16 20:53:33 -0400

    Created Hook to get concert attendees

--- server/api/index.js
+++ server/api/index.js
@@ -6,11 +6,13 @@ const auth = require('./auth');
 // TODO: will need for creating new users
 const user = require('./user');
 const genre = require('./genre');
+const concert = require('./concerts');
 
 router.use('/ticketmaster', ticketmaster);
 router.use('/spotify', spotify);
 router.use('/auth', auth);
-router.use('/genre', genre)
+router.use('/genre', genre);
+router.use('/concerts', concert);
 
 // TODO: will need for creating new users
 router.use('/user', user);
