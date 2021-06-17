998035c1294d1cc07c0231de31c385cb7c008a0a
Craig Ferreira <ferreira.craig@gmail.com>
2021-06-17 19:58:01 -0400

    single event page

--- client/Components/Card.jsx
+++ client/Components/Card.jsx
@@ -1,21 +1,13 @@
 import React, { useContext, useEffect, useState } from 'react';
+import { CssBaseline, Typography, AppBar, Card, CardActions, CardActionArea, CardMedia, CardContent, Container, Grid, Box } from '@material-ui/core';
 import {makeStyles} from '@material-ui/core/styles'
-import Card from '@material-ui/core/Card'
-import CardActionArea from '@material-ui/core/CardActionArea'
-import CardActions from '@material-ui/core/CardActions'
-import CardContent from '@material-ui/core/CardContent'
-import CardMedia from '@material-ui/core/CardMedia'
-import Button from '@material-ui/core/Button'
-import Typography from '@material-ui/core/Typography'
-import Paper from '@material-ui/core/Paper'
 import concertBackground from '../../public/concertBackground.png'
-import IconButton from '@material-ui/core/IconButton';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
 import {GlobalState} from '../contexts/Store'
-import clsx from 'clsx';
+import { styled } from "@material-ui/core/styles";
+import { spacing } from "@material-ui/system";
+import MuiButton from "@material-ui/core/Button";
+
+const Button = styled(MuiButton)(spacing)
 
 const useStyles =  makeStyles((theme) =>({
   CardActionArea: {
@@ -39,60 +31,106 @@ mainFeaturedPostContent: {
       padding: theme.spacing(6),
       paddingRight: 0,
     }
-}
+},
+leftBox:{
+    backgroundColor: 'black',
+    height: '300px',
+    padding: '30px'
+},
+rightBox:{
+
+},
+button: {
+    margin: theme.spacing(1),
+    borderRadius: "5em"
+  },
+  element:{
+    backgroundColor: '#311b92',
+    height: '300px',
+    padding: '30px',
+    margin: '20px',
+    direction:"row",
+    justify:"center",
+    alignItems:"center",
+    padding: '30px'
+  },
+  mainGrid:{
+    direction:"row",
+    justify:"center",
+    alignItems:"center",
+    paddingLeft: '100px',
+  }
+
 }));
 
 export default function Cards(props){
-    const [expanded, setExpanded] = useState(false);
+    const [count, setCount]= useState(true)
 
-    const handleExpandClick = () => {
-        setExpanded(!expanded);
-    };
+    const convertTime=(time)=>{
 
+        time = time.split(':'); 
+
+        var hours = Number(time[0]);
+        var minutes = Number(time[1]);
+        var seconds = Number(time[2]);
+
+        var timeValue;
+
+        if (hours > 0 && hours <= 12) {
+        timeValue= "" + hours;
+        } else if (hours > 12) {
+        timeValue= "" + (hours - 12);
+        } else if (hours == 0) {
+        timeValue= "12";
+        }
+        
+        timeValue += (minutes < 10) ? ":0" + minutes : ":" + minutes;  
+        timeValue += (hours >= 12) ? " P.M." : " A.M."; 
+        return timeValue
+    }
+
+
+    const getWorkingImage=(imageArr)=>{
+        let workingImages=[]
+        console.log('image arr', imageArr)
+        for(let image of imageArr){
+            if(image.ratio==='3_2') return image.url
+        }
+    }
 
     const classes = useStyles()
-    console.log('props', props.props)
-    return <Paper style={{backgroundImage: useStyles.paperContainer}}>
-        <div className={useStyles.mainFeaturedPost} style={{height: 400, position:'relative',backgroundSize: 'cover',  backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundImage: `url('https://static01.nyt.com/images/2020/03/12/arts/12virus-concerts2/merlin_170369790_63d02a4c-82f8-441a-ac7c-c395828b7961-superJumbo.jpg?quality=90&auto=webp')`}}><div><Typography variant='h1'>{props.props.name}</Typography></div>{props.props.dates?<div><Typography variant='h3'>Date: {props.props.dates.start.localDate}</Typography>{props.props?<Typography variant='h3'>Venue: {props.props._embedded.venues[0].name}</Typography>:null}</div>:null}</div>
-    <div style={{width: 500, display: 'flex'}}>
-    <Card >
-        <CardActionArea style={useStyles.CardActionArea}>
-            <CardContent style={{color: 'black'}}>
-                {props.props.images? props.props.images.map((curr, idx)=>{
-                    if(idx<2) return <CardMedia key={idx} style={{height: 200, width: 500, display: 'flex'}} image={curr.url}/>
-                }): null}
-            </CardContent>
-            <CardContent style={{color: 'black'}}>Concert Name: {props?props.props.name: null}</CardContent>
-            {props.props.classifications?<CardContent style={{color: 'black'}}>Genre: {props.props.classifications[0].genre.name}</CardContent>: null}
-            {props.props?<CardContent style={{color: 'black'}}>Location {props.props._embedded? props.props._embedded.venues[0].name: null}</CardContent>: null}
-            {props.props.dates?<CardContent style={{color: 'black'}}>Date: {props.props.dates.start.localDate}</CardContent>:null}
-            {props.props.dates?<CardContent style={{color: 'black'}}>Time: {props.props.dates.start.localTime}</CardContent>:null}
-            {props.props.priceRanges?<CardContent style={{color: 'black'}}>Price Range: ${props.props.priceRanges[0].min} to ${props.props.priceRanges[0].max}</CardContent>:null}
-            {props.props.info? <CardContent style={{color: 'black'}}>Details: {props.props.info}</CardContent>:null}
-        </CardActionArea>
-        {props.props.url?<Button style={{color: 'black'}}><a href={props.props.url}>View Seats</a></Button>: null}
-        
-        
-        <CardActions disableSpacing>
-                    <IconButton aria-label="add to favorites">
-                        <FavoriteIcon />
-                    </IconButton>
-                    <IconButton aria-label="share">
-                        <ShareIcon />
-                    </IconButton>
-                    <IconButton
-                        className={clsx(classes.expand, {
-                            [classes.expandOpen]: expanded,
-                        })}
-                        onClick={handleExpandClick}
-                        aria-expanded={expanded}
-                        aria-label="show more"
-                    >
-                        <ExpandMoreIcon />
-                    </IconButton>
-        </CardActions>
 
-    </Card>
-    </div>
-    </Paper>
+    return <>
+        <CssBaseline/>
+        <main>
+            <div display='flex'>
+   `            <Grid container>
+                    <Grid item md={8} className={classes.leftBox}>
+                        <Container>
+
+                            <Typography variant='h3'  color='textPrimary' gutterBottom bgcolor="text.primary">{props.props.name}</Typography>
+                            {props.props.dates?<Typography variant='h5' color='textPrimary' gutterBottom bgcolor="text.primary">{props.props.dates.start.localDate}</Typography>: null}
+                            {props.props.url?<Button margin='30px' variant="contained" color="primary"><a href={props.props.url}>View Seats</a></Button>: null}
+                            {props.props.url?<Button variant="outlined" color="primary">I'm Attending</Button>: null}
+                        </Container>
+                    </Grid>
+
+                    {props.props.images? <Grid item md={4} className={classes.rightBox} ><img height='300px' src={getWorkingImage(props.props.images)} /></Grid>: null}
+
+                </Grid>
+
+            </div>
+            <div>
+                <Grid container  className={classes.mainGrid} >
+                    <Grid item xs={3} className={classes.element} >
+                        <Typography variant='h4'  color='textPrimary' gutterBottom bgcolor="text.primary">Venue Details</Typography>
+                        {props.props._embedded?<Typography variant='h6'  color='textPrimary' gutterBottom bgcolor="text.primary">Venue: {props.props._embedded.venues[0].name}</Typography>: null}
+                        {props.props.dates?<Typography variant='h6'  color='textPrimary' gutterBottom bgcolor="text.primary">Start Time: {convertTime(props.props.dates.start.localTime)}</Typography>: null}
+                    </Grid>
+                    <Grid item xs={3} className={classes.element} ></Grid>
+                    <Grid item xs={3} className={classes.element} ></Grid>
+                </Grid>
+            </div>
+        </main>
+    </>
 }
