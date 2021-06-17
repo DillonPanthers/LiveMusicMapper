23efdf734de1a3447e62ad75fca57ee59b15eef2
Vikki Chan <vikkiwchan@gmail.com>
2021-06-17 13:51:31 -0500

    refactored FilterMap to use functions from utils

--- client/Components/SecondNavBar/FilterMap.jsx
+++ client/Components/SecondNavBar/FilterMap.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useContext, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
 import { MenuItem, Menu, makeStyles } from '@material-ui/core';
 import axios from 'axios';
 
 import { TICKETMASTERAPIKEY } from '../../secret';
 import { GlobalState } from '../../contexts/Store';
+import { getVenueObject, getEvents } from './utils';
 
 import OutlinedButton from '../StyledComponents/OutlinedButton';
 
@@ -30,8 +30,6 @@ const useStyles = makeStyles((theme) => ({
 //Use curr location even when dragging and zooming
 
 const Filter = () => {
-    const currLocation = useLocation();
-    const path = currLocation.pathname;
     let classes = useStyles();
 
     const [menuOpen, setMenuOpen] = useState(false);
@@ -59,9 +57,7 @@ const Filter = () => {
 
     useEffect(() => {
         const getGenres = async () => {
-            // console.log('hello');
             const genreList = await axios.get(`/api/genre`);
-            // console.log('genres', genreList.data);
             setGenres(genreList.data);
         };
         getGenres();
@@ -69,101 +65,66 @@ const Filter = () => {
 
     const filterMapData = async (event) => {
         const { myValue } = event.currentTarget.dataset;
+
+        const events = await getEvents(
+            locationData,
+            radius,
+            TICKETMASTERAPIKEY,
+            myValue
+        );
+
+        const venueObj = await getVenueObject(events);
+
         setGenre(myValue);
-        const latlong = locationData.lat + ',' + locationData.lon;
-
-        const ticketDataByLocation = await axios.get(
-            `https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&genreId=${myValue}&size=200&latlong=${latlong}&radius=${radius}&apikey=${TICKETMASTERAPIKEY}`
-        );
-
-        /**
-         * I am taking the json from the ticketmaster data, manipulating it and setting our global state concerts field, which we should probably rename venues.
-         *
-         * In venueObj below, I am taking the data and setting up an object that has just the venues as the keys, and the values of each of the keys,
-         * will also be an object with two keys, one being venueData which would be the data of the current venue, and the second being a venueEvents, which
-         * I am storing in a set because I didn't really want to have that as another object key value pair.
-         */
-
-        const venueObj = ticketDataByLocation.data._embedded.events.reduce(
-            (accum, event) => {
-                const venueName = event._embedded.venues[0].name;
-                if (!accum.hasOwnProperty(venueName)) {
-                    const venueData = event._embedded.venues[0];
-                    accum[venueName] = {
-                        venueData: venueData,
-                        venueEvents: [],
-                    };
-                }
-                return accum;
-            },
-            {}
-        );
-
-        /**
-         * Now that I have an object that would look something like
-         * {
-         *    Barclays Center: { venueData: {data of venue details here}, venueEvents: {will be a set that has the concerts at this venue} }
-         * }
-         *
-         * Below I am modifying those fields. For each of the events I am populating the concerts part of the venue in the object I already set up above.
-         *
-         */
-        ticketDataByLocation.data._embedded.events.forEach((event) => {
-            const eventVenue = event._embedded.venues[0].name;
-            venueObj[eventVenue].venueEvents.push(event);
-        });
-
         setVenues(venueObj);
         setMapView('');
         setPersonalized(false);
     };
 
-    {
-        return path === '/map' ? (
-            <>
-                <OutlinedButton
-                    className={classes.button}
-                    variant="outlined"
-                    aria-controls="simple-menu"
-                    aria-haspopup="true"
-                    onClick={recordButtonPosition}
-                >
-                    FILTER&nbsp;BY&nbsp;GENRE
-                </OutlinedButton>
-                <Menu
-                    id="simple-menu"
-                    className={classes.menu}
-                    anchorEl={anchorEl}
-                    keepMounted
-                    open={Boolean(menuOpen)}
-                    onClose={closeMenu}
-                    elevation={0}
-                    getContentAnchorEl={null}
-                    anchorOrigin={{
-                        vertical: 'bottom',
-                        horizontal: 'center',
-                    }}
-                    transformOrigin={{
-                        vertical: 'top',
-                        horizontal: 'center',
-                    }}
-                >
-                    {genreList.map((curr) => {
-                        return (
-                            <MenuItem
-                                data-my-value={curr.id}
-                                className={classes.container}
-                                onClick={filterMapData}
-                                key={curr.id}
-                            >
-                                {curr.name}
-                            </MenuItem>
-                        );
-                    })}
-                </Menu>
-            </>
-        ) : null;
-    }
+    return (
+        <>
+            <OutlinedButton
+                className={classes.button}
+                variant="outlined"
+                aria-controls="simple-menu"
+                aria-haspopup="true"
+                onClick={recordButtonPosition}
+            >
+                FILTER&nbsp;BY&nbsp;GENRE
+            </OutlinedButton>
+            <Menu
+                id="simple-menu"
+                className={classes.menu}
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(menuOpen)}
+                onClose={closeMenu}
+                elevation={0}
+                getContentAnchorEl={null}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                }}
+            >
+                {genreList.map((curr) => {
+                    return (
+                        <MenuItem
+                            data-my-value={curr.id}
+                            className={classes.container}
+                            onClick={filterMapData}
+                            key={curr.id}
+                        >
+                            {curr.name}
+                        </MenuItem>
+                    );
+                })}
+            </Menu>
+        </>
+    );
 };
 
 export default Filter;
