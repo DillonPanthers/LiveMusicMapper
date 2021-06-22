dbd83999fb45e280476c0383dd1cfb1b96e03bc2
Alejandra Villanueva <alejyvilla@gmail.com>
2021-06-22 18:59:07 -0400

    Removed FriendRequest Model and other db unused code

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,7 @@
 const { db } = require('./db');
 
 //import models from hooks.js
-const { Concert, Genre, User, Friendship, FriendRequest } = require('./hooks');
+const { Concert, Genre, User, Friendship } = require('./hooks');
 
 ///export models and db into the seed file
-module.exports = { db, Concert, Genre, User, Friendship, FriendRequest };
+module.exports = { db, Concert, Genre, User, Friendship };
--- server/db/relationships.js
+++ server/db/relationships.js
@@ -3,11 +3,6 @@ const { Concert } = require('./models/Concert');
 const { Genre } = require('./models/Genre');
 const { User } = require('./models/User');
 const { Friendship } = require('./models/Friendship');
-const { FriendRequest } = require('./models/FriendRequest');
-
-// Concert Genres
-Concert.belongsToMany(Genre, { through: 'concertgenres', timestamps: false });
-Genre.belongsToMany(Concert, { through: 'concertgenres', timestamps: false });
 
 // User Concerts
 Concert.belongsToMany(User, {
@@ -35,32 +30,5 @@ User.belongsToMany(User, {
 Friendship.belongsTo(User, { as: 'userInfo', foreignKey: 'userId' });
 Friendship.belongsTo(User, { as: 'friendInfo', foreignKey: 'friendId' });
 
-/*
-// Friend Request System
-
-// User.findAll({ include: 'invitees' })) -> find people User has invited/requested
-// instance.getInvitees() -> get an instances invitees / person they have requested
-User.belongsToMany(User, {
-    as: 'invitees',
-    through: FriendRequest,
-    foreignKey: 'requesterId',
-    otherKey: 'inviteeId',
-});
-
-// User.findAll({ include: 'requestedBy' })) -> find people who have requested you
-// instance.getRequestedBy()
-User.belongsToMany(User, {
-    as: 'requestedBy',
-    through: FriendRequest,
-    foreignKey: 'inviteeId',
-    otherKey: 'requesterId',
-});
-
-// FriendRequest.findAll({ include: [{model: User, as: "invitee" or "requestedBy"}]})
-// Use FriendRequest table to find info on invitee and requester
-FriendRequest.belongsTo(User, { as: 'invitee', foreignKey: 'inviteeId' });
-FriendRequest.belongsTo(User, { as: 'requestedBy', foreignKey: 'requesterId' });
-*/
-
 // After relationships are created, export them here into hooks.js
-module.exports = { Concert, Genre, User, Friendship, FriendRequest };
+module.exports = { Concert, Genre, User, Friendship };
